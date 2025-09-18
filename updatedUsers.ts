3273edf9a38a1f82b99a624a7e2bdc64253438b9
angenoella@gmail.com <angenoella@gmail.com>
2025-09-18 12:08:53 +0200

    Initial commit

---
+++ updatedUsers.ts
@@ -0,0 +1,56 @@
+type User = {
+    id: number;
+    fullname: string;
+    email: string;
+    phoneNumber: string;
+    age: number;
+    physicalAddress: string;
+
+}
+const Users: User[] =
+    [
+        {
+
+            id: 101,
+            fullname: "Noella",
+            email: "noella@gmail.com",
+            phoneNumber: "+250780456623",
+            age: 30,
+            physicalAddress: "Kigali"
+        },
+        {
+            id: 102,
+            fullname: "Ange",
+            email: "mutesideno@gmail.com",
+            phoneNumber: "+250789456723",
+            age: 29,
+            physicalAddress: "Los Angeles"
+        },
+        {
+            id: 103,
+            fullname: "Mutesi",
+            email: "mutesi@gmail.com",
+            phoneNumber: "+250788032765",
+            age: 27,
+            physicalAddress: "Zanzibar"
+        },
+        {
+            id: 104,
+            fullname: "Agape",
+            email: "agape@gmail.com",
+            phoneNumber: "+250781254890",
+            age: 25,
+            physicalAddress: "Nairobi"
+        },
+        {
+            id: 105,
+            fullname: "Grace",
+            email: "Grace@gmail.com",
+            phoneNumber: "+250782784503",
+            age: 20,
+            physicalAddress: "Dubai"
+        }
+
+    ]
+
+    console.log(Users);
---
+++ users.js
@@ -0,0 +1,45 @@
+const users =
+    [
+        {
+            id: 101,
+            fullname: "Noella",
+            email: "noella@gmail.com",
+            phoneNumber: "+250780456623",
+            age: 30,
+            physicalAddress: "Kigali"
+        },
+        {
+            id: 102,
+            fullname: "Ange",
+            email: "mutesideno@gmail.com",
+            phoneNumber: "+250780456623",
+            age: 29,
+            physicalAddress: "Los Angeles"
+        },
+        {
+            id: 103,
+            fullname: "Mutesi",
+            email: "mutesi@gmail.com",
+            phoneNumber: "+250780456623",
+            age: 27,
+            physicalAddress: "Zanzibar"
+        },
+        {
+            id: 104,
+            fullname: "Agape",
+            email: "agape@gmail.com",
+            phoneNumber: "+250780456623",
+            age: 25,
+            physicalAddress: "Nairobi"
+        },
+        {
+            id: 105,
+            fullname: "Grace",
+            email: "Grace@gmail.com",
+            phoneNumber: "+250780456623",
+            age: 20,
+            physicalAddress: "Dubai"
+        }
+    ];
+
+console.log(users);
