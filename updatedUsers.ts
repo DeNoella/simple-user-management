0d3a2c8beef6433143f887103c63db3ab6110ff0
angenoella@gmail.com <angenoella@gmail.com>
2025-09-18 17:31:27 +0200

    Refactoring users code

--- updatedUsers.ts
+++ updatedUsers.ts
@@ -1,76 +1,74 @@
-(() => {
-    type User = {
-        id: number;
-        fullname: string;
-        email: string;
-        phoneNumber: string;
-        age: number;
-        physicalAddress: string;
+type User = {
+    id: number;
+    fullname: string;
+    email: string;
+    phoneNumber: string;
+    age: number;
+    physicalAddress: string;
+}
+
+const users: User[] = [
+    {
+        id: 101,
+        fullname: "Noella",
+        email: "noella@gmail.com",
+        phoneNumber: "+250780456623",
+        age: 30,
+        physicalAddress: "Kigali"
+    },
+    {
+        id: 102,
+        fullname: "Ange",
+        email: "mutesideno@gmail.com",
+        phoneNumber: "+250789456723",
+        age: 29,
+        physicalAddress: "Los Angeles"
+    },
+    {
+        id: 103,
+        fullname: "Mutesi",
+        email: "mutesi@gmail.com",
+        phoneNumber: "+250788032765",
+        age: 27,
+        physicalAddress: "Zanzibar"
+    },
+    {
+        id: 104,
+        fullname: "Agape",
+        email: "agape@gmail.com",
+        phoneNumber: "+250781254890",
+        age: 25,
+        physicalAddress: "Nairobi"
+    },
+    {
+        id: 105,
+        fullname: "Grace",
+        email: "Grace@gmail.com",
+        phoneNumber: "+250782784503",
+        age: 20,
+        physicalAddress: "Dubai"
     }
+];
 
-    const users: User[] = [
-        {
-            id: 101,
-            fullname: "Noella",
-            email: "noella@gmail.com",
-            phoneNumber: "+250780456623",
-            age: 30,
-            physicalAddress: "Kigali"
-        },
-        {
-            id: 102,
-            fullname: "Ange",
-            email: "mutesideno@gmail.com",
-            phoneNumber: "+250789456723",
-            age: 29,
-            physicalAddress: "Los Angeles"
-        },
-        {
-            id: 103,
-            fullname: "Mutesi",
-            email: "mutesi@gmail.com",
-            phoneNumber: "+250788032765",
-            age: 27,
-            physicalAddress: "Zanzibar"
-        },
-        {
-            id: 104,
-            fullname: "Agape",
-            email: "agape@gmail.com",
-            phoneNumber: "+250781254890",
-            age: 25,
-            physicalAddress: "Nairobi"
-        },
-        {
-            id: 105,
-            fullname: "Grace",
-            email: "Grace@gmail.com",
-            phoneNumber: "+250782784503",
-            age: 20,
-            physicalAddress: "Dubai"
-        }
-    ];
+function myProfile(): User | undefined {
+    const id = 103;
+    return users.find(user => user.id === id);
+}
 
-    function myProfile(): User | undefined {
-        const id = 103;
-        return users.find(user => user.id === id);
-    }
+function lessAge(): User | undefined {
+    const ageLimit = 27;
+    return users.find(user => user.age < ageLimit);
+}
 
-    function lessAge(): User | undefined {
-        const ageLimit = 27;
-        return users.find(user => user.age < ageLimit);
-    }
+function getUsers(): User[] {
+    return users;
+}
 
-    function getUsers(): User[] {
-        return users;
-    }
-
-    const user = myProfile();
-    console.log(user);
-    console.log("===============");
-    const youngUser = lessAge();
-    console.log(youngUser);
-    console.log("===============");
-    const allUsers = getUsers();
-    console.log(allUsers);
-})();
+const user = myProfile();
+console.log(user);
+console.log("===============");
+const youngUser = lessAge();
+console.log(youngUser);
+console.log("===============");
+const allUsers = getUsers();
+console.log(allUsers);
